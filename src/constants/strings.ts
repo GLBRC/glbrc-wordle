3b7de7b23f7f925e57ccdac8fa458d1f55c2811f
Clint Cecil <ccecil@wisc.edu>
2022-04-14 13:39:51 -0500

    Add hashtag to share text

--- src/constants/strings.ts
+++ src/constants/strings.ts
@@ -26,4 +26,4 @@ export const SUCCESS_RATE_TEXT = 'Success rate'
 export const CURRENT_STREAK_TEXT = 'Current streak'
 export const BEST_STREAK_TEXT = 'Best streak'
 export const CLIPBOARD_MESSAGE_TEXT =
-  'Brought to you by the GLBRC IS Department. Come see us during the poster session!'
+  'Brought to you by the GLBRC IS Department. Come see us during the poster session! #GLBRSCIENCE'
