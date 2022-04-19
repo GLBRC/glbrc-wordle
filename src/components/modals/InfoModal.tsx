601c19b9a4101ad101f04d50cc8084ccdbc1ffcc
Clint Cecil <ccecil@wisc.edu>
2022-04-19 15:42:56 -0500

    Branding

--- src/components/modals/InfoModal.tsx
+++ src/components/modals/InfoModal.tsx
@@ -10,6 +10,62 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="How to play" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500 dark:text-gray-300">
+        This is an open source version of the word guessing game we all know and
+        love made for the ASM. Come by the IS Poster Area during the poster
+        session to see more cool projects and feel free to -{' '}
+        <a
+          href="https://github.com/thatRailsGuy/glbrc-wordle"
+          className="underline font-bold"
+        >
+          check out the code here
+        </a>{' '}
+      </p>
+      <br />
+      <p className="text-sm text-gray-500 dark:text-gray-300">
+        Made by the IS department for the
+      </p>
+
+      <div className="flex justify-center mb-1 mt-4">
+        <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="G"
+          status="correct"
+        />
+        <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="L"
+          status="present"
+        />
+        <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="B"
+          status="correct"
+        />
+        <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="R"
+          status="present"
+        />
+        <Cell
+          isRevealing={true}
+          isCompleted={true}
+          value="C"
+          status="correct"
+        />
+      </div>
+
+      <br />
+      <h3
+        className="text-lg leading-6 font-medium text-gray-900 dark:text-gray-100"
+        id="headlessui-dialog-title-37"
+      >
+        How to play
+      </h3>
+      <p className="text-sm text-gray-500 dark:text-gray-300">
         Guess the word in 6 tries. After each guess, the color of the tiles will
         change to show how close your guess was to the word.
       </p>
@@ -55,18 +111,6 @@ export const InfoModal = ({ isOpen, handleClose }: Props) => {
       </div>
       <p className="text-sm text-gray-500 dark:text-gray-300">
         The letter U is not in the word in any spot.
-      </p>
-
-      <p className="mt-6 italic text-sm text-gray-500 dark:text-gray-300">
-        This is an open source version of the word guessing game we all know and
-        love made for the ASM. Come by the IS Poster Area during the poster
-        session to see more cool projects and feel free to -{' '}
-        <a
-          href="https://github.com/thatRailsGuy/glbrc-wordle"
-          className="underline font-bold"
-        >
-          check out the code here
-        </a>{' '}
       </p>
     </BaseModal>
   )
