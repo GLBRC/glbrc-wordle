e8179f0a3bc8bfbd1e11fe4bd57f597d3c531527
Clint Cecil <ccecil@wisc.edu>
2022-05-06 09:28:01 -0500

    Start on ASM Day

--- src/lib/words.ts
+++ src/lib/words.ts
@@ -75,15 +75,17 @@ export const localeAwareUpperCase = (text: string) => {
 }
 
 export const getWordOfDay = () => {
-  // January 1, 2022 Game Epoch
-  const epochMs = new Date(2022, 0).valueOf()
+  // May 17, 2022 Game Epoch
+  const epochMs = new Date(2022, 5, 17, 0).valueOf()
   const now = Date.now()
   const msInDay = 14400000
   const index = Math.floor((now - epochMs) / msInDay)
   const nextday = (index + 1) * msInDay + epochMs
 
   return {
-    solution: localeAwareUpperCase(WORDS[index % WORDS.length]),
+    solution: localeAwareUpperCase(
+      WORDS[((index % WORDS.length) + WORDS.length) % WORDS.length]
+    ),
     solutionIndex: index,
     tomorrow: nextday,
   }
